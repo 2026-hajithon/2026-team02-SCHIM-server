2e7a8b49967e34a7dbbeae40773cfc5cc8f9f793
phdcoco <claireluna470@gmail.com>
2026-08-01 20:05:13 +0900

    chore: initializer

---
+++ src/main/java/com/hajithon/schim/SchimApplication.java
@@ -0,0 +1,13 @@
+package com.hajithon.schim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hajithon/schim/SchimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hajithon.schim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
